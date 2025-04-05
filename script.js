5855031ff409fb51421c3944c1d8df0cac0287ca
abhishekbaraik333 <abhitamangme@gmail.com>
2025-04-06 01:07:20 +0530

    okay

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-  navLinks = document.querySelectorAll(".nav-link");
-  const mobileMenu = document.querySelector(".mobile-menu");
-  const mobileMenuIcon = document.querySelector(".menu-icon");
-  navLinks.forEach((nav) => {
-    nav.addEventListener("click", function () {
-      alert("Welcome to my Portfolio!")
-      mobileMenu.classList.remove("active")
+    let navLinks = document.querySelectorAll(".nav-link"); // 👈 add `let`
+    const mobileMenu = document.querySelector(".mobile-menu");
+    const mobileMenuIcon = document.querySelector(".menu-icon");
+  
+    navLinks.forEach((nav) => {
+      nav.addEventListener("click", function () {
+        alert("Welcome to my Portfolio!");
+        mobileMenu.classList.remove("active");
+      });
+    });
+  
+    mobileMenuIcon.addEventListener("click", function () {
+      mobileMenu.classList.toggle("active");
     });
   });
-
-  mobileMenuIcon.addEventListener("click", function () {
-    mobileMenu.classList.toggle("active");
-  });
-});
+  
